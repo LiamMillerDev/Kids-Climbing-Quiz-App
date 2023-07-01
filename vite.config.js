02511a784f1e3bb4d299dc446624b7e252bf337c
Liam Miller <LiamJmillerNZ@gmail.com>
2023-07-01 16:42:51 +1200

    Fix error

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mountain-whiz/",
   build: {
-    outDir: "dist",
+    base: "./",
+    assetsDir: "assets",
   },
 });
