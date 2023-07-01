e854ce0ba258812ece9d607cf2c09702ba68052a
Liam Miller <LiamJmillerNZ@gmail.com>
2023-07-01 16:36:14 +1200

    updated gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/mountain-whiz/", // This line was added
+  build: {
+    outDir: "dist", // And this line too
+  },
+});
